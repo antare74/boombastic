4cf6203d0ba884bfa0f2a49bb95a035ce9751ddc
rajifmahendra <rajifmahendra10@gmail.com>
2020-07-19 21:24:06 +0700

    layouting user dan admin sudah di pisah

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+// Route untuk admin
+Route::prefix('admin')->namespace('Admin')->group(function(){     
+        Route::get('/', 'AdminController@index');
+});
