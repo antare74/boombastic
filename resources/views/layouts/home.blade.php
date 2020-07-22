2826df7d65e26cff559217e301991bfb8dff5128
unknown <adit.goyo@gmail.com>
2020-07-22 16:16:13 +0700

    benerin responsif

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,73 +13,65 @@
     @include('includes.user.sidebar')
     @include('includes.user.header')
 
-    <div class="hero w-100">
-        <div class="row">
-            <div class="col-md-12 text-center py-5">
-                <div class="logo-container">
-                    <img src="/frontend/img/boom_logo.svg" class="mx-auto d-block logo_big" alt="...">
-                </div>
+<div class="container-fluid w-screen mt-3">
+    
+    {{-- HOME start --}}
+    <div class="home-section">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="intro container text-center">
+                <img src="/frontend/img/logo.svg" class="mx-auto d-block" alt="">
             </div>
         </div>
     </div>
+    {{-- HOME end --}}
 
+    {{-- Location Start --}}
+    <div class="location-section">
+        <div id="paralax-image">
+        </div>
+        <div class="container py-2 pb-3">
+            <div class="location text-center">
+                <h1 class="pb-3"><span>Locations</span></h1>
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3 text-left my-3">
+                        <h4>Jl. Gading Serpong Boulevard No.26</h4>
+                        <p><b>Sunday - Saturday , 10am - 9pm</b></p>
+                        <a href=""><i class="fas fa-map-marked fa-2x"></i> Map</a>
 
-
-
-    <div id="paralax-image">
-    </div>
-    <div class="container py-5">
-        <div class="row h-100">
-            <div class="col-md-12 align-self-center">
-                <div class="location text-center">
-                    <h1 class="pb-5">Locations</h1>
-                    <div class="row">
-                        <div class="col-md-6 pb-5">
-                            <h4>Jl. Gading Serpong Boulevard No.26</h4>
-                            <p><b>Sunday - Saturday , 10am - 9pm</b></p>
-                            <a href=""><i class="fas fa-map-marked fa-2x"></i></a>
-
-
-                        </div>
-                        <div class="col-md-6">
-                            <h4>Jl. Kecubung Raya Alfamart Harapan Kita 3</h4>
-                            <p><b>Sunday - Saturday , 10am - 9pm</b></p>
-                            <a href=""><i class="fas fa-map-marked fa-2x"></i></a>
-
-                        </div>
                     </div>
+                    <div class="col-sm-3 text-left my-3">
+                        <h4>Jl. Kecubung Raya Alfamart Harapan Kita 3</h4>
+                        <p><b>Sunday - Saturday , 10am - 9pm</b></p>
+                        <a href=""><i class="fas fa-map-marked fa-2x"></i> Map</a>
+                    </div>
+                    <div class="col-md-3"></div>
                 </div>
             </div>
         </div>
-
     </div>
+    {{-- Location End --}}
 
-    <div id="paralax-image-2" class="img-fluid max-width: 100% height: auto;">
-        <div class="row h-100">
-            <div class="col-md-12 align-self-center">
-                <div class="card text-center" style="width: 25rem;">
+    {{-- Ourmenu --}}
+    <div class="ourmenu-section text-center">
+        <div class="jumbotron jumbotron-fluid">
+
+            <div class="intro">
+                <div class="card mx-auto d-flex" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Treat Yourself</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn modal-btn">Check Our Menu >>></a>
+                        <a href="#" class="btn menu-btn">Check Ourmenu >>></a>
                     </div>
                 </div>
+
             </div>
+
         </div>
     </div>
-    {{-- <!-- modal -->
-    <div class="modal-overlay">
-        <div class="modal-container">
-            <h3>modal content</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem similique molestiae labore
-                maxime
-                necessitatibus consectetur enim vero assumenda iusto voluptas.</p>
-            <button class="close-button">
-                <i class="fas fa-times"></i>
-            </button>
-        </div>
-    </div> --}}
+    {{-- Ourmenu End --}}
 
+</div>
     @include('includes.user.footer')
 
 
